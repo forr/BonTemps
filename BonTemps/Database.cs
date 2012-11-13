022f4e1b479db80ef20b265cfdcbf58895d4cf97
Alain <bressers.a@gmail.com>
2012-11-13 11:29:46 +0100

    Version 1.03 Beta

--- BonTemps/Database.cs
+++ BonTemps/Database.cs
@@ -11,42 +11,121 @@ namespace BonTemps
 {
     public static class Database
     {
+        private enum TableName { Tables, Clients, Orders, TableOrders, Menus, Persons };
+
         private static string GetConnectionString()
         {
             return global::BonTemps.Properties.Settings.Default.DataConnectionString;
         }
-        public static bool Insert(Tables t, Clients c, Orders o, TableOrders to, Menus m, Persons p)
+
+        public static bool Insert(string[] tableNames, string[] values)
         {
-            string sqlStatement = "INSERT INTO Tables,Clients,Orders,TableOrders,Menus,Persons";
+            string sqlCmd = String.Empty;
+            string tables = String.Empty;
+            string statement = String.Empty;
+            int selectIndex = 0;
+            foreach (String str in tableNames)
+            {
+                foreach (String str2 in values)
+                {
+                    if (selectIndex == 0) statement += String.Format("INSERT INTO {0} VALUES({1})", str, str2);
+                }
+            }
+        }
+        public static bool Update(TableName tableName, string[] argsCol, string[] argsVal, int id)
+        {
+            string sqlCmd = String.Empty;
+            string table = String.Empty;
+            string selectColumns = String.Empty;
+            int selectIndex = 0;
+            switch (tableName)
+            {
+                case TableName.Tables:
+                    table = "Tables";
+                    break;
+                case TableName.Clients:
+                    table = "Clients";
+                    break;
+                case TableName.Orders:
+                    table = "Orders";
+                    break;
+                case TableName.TableOrders:
+                    table = "TableOrders";
+                    break;
+                case TableName.Menus:
+                    table = "Menus";
+                    break;
+                case TableName.Persons:
+                    table = "Persons";
+                    break;
+            }
+            foreach (String str in argsCol)
+            {
+                foreach (String str2 in argsVal)
+                {
+                    if (selectIndex == 0) selectColumns += String.Format("{0}='{1}'", str, str2);
+                    else selectColumns += String.Format(",{0}='{1}'", str, str2);
+                    selectIndex++;
+                }
+            }
+
+            sqlCmd = "UPDATE @table SET @statement WHERE ID=@id";
+
             try
             {
                 using (SqlConnection sqlConn = new SqlConnection(GetConnectionString()))
                 {
-                    sqlConn.Open();
-                    if (sqlConn.State == ConnectionState.Open)
-                    {
-                        SqlCommand sqlQuery = new SqlCommand(sqlStatement, sqlConn);
-                        int m_success = sqlQuery.ExecuteNonQuery();
-                        return m_success == 1;
-                    }
+                    SqlCommand sqlQuery = new SqlCommand(sqlCmd, sqlConn);
+                    sqlQuery.Parameters.AddWithValue("@table", table);
+                    sqlQuery.Parameters.AddWithValue("@statement", selectColumns);
+                    sqlQuery.Parameters.AddWithValue("@id", id);
+                    int success = sqlQuery.ExecuteNonQuery();
+                    return success == 1;
                 }
-                return false;
             }
             catch { return false; }
         }
-
-        public static bool Update(Tables t, Clients c, Orders o, TableOrders to, Menus m, Persons p)
+        public static bool Delete(TableName tableName, int id)
         {
-            string sqlStatement = "UPDATE Tables,Clients,Orders,TableOrders,Menus,Persons";
+            string sqlCmd = String.Empty;
+            string table = String.Empty;
+            string selectColumns = String.Empty;
+            switch (tableName)
+            {
+                case TableName.Tables:
+                    table = "Tables";
+                    break;
+                case TableName.Clients:
+                    table = "Clients";
+                    break;
+                case TableName.Orders:
+                    table = "Orders";
+                    break;
+                case TableName.TableOrders:
+                    table = "TableOrders";
+                    break;
+                case TableName.Menus:
+                    table = "Menus";
+                    break;
+                case TableName.Persons:
+                    table = "Persons";
+                    break;
+            }
+
+            sqlCmd = "DELETE FROM @table WHERE ID=@id";
+
             try
             {
-                return false;
+                using (SqlConnection sqlConn = new SqlConnection(GetConnectionString()))
+                {
+                    SqlCommand sqlQuery = new SqlCommand(sqlCmd, sqlConn);
+                    sqlQuery.Parameters.AddWithValue("@table", table);
+                    sqlQuery.Parameters.AddWithValue("@id", id);
+                    int success = sqlQuery.ExecuteNonQuery();
+                    return success == 1;
+                }
             }
             catch { return false; }
-        }
-        public static bool Delete(Tables t, Clients c, Orders o, TableOrders to, Menus m, Persons p)
-        {
-            return false;
         }
         public static DataTable Select()
         {
@@ -60,31 +139,7 @@ namespace BonTemps
         //R version
         #region Example1
         /*
-        public enum TableSelect { CLIENTS, TABLES, ORDERS }
-
-        public static DataTable SelectGlobal(TableSelect tableSelect, string[] ArgsSelect, string ArgsWhere)
-        {
-            string sqlcmd = String.Empty;
-            string table = String.Empty;
-            string selectables = String.Empty;
-            DataTable dt = null;
-
-            switch(tableSelect)
-            {
-                case TableSelect.CLIENTS:
-                    table = "'Clients'";
-                    break;
-                case TableSelect.ORDERS:
-                    table = "'Orders'";
-                    break;
-                case TableSelect.TABLES:
-                    table = "'Tables'";
-                    break;
-            }
-            foreach(String s in ArgsSelect)
-            {
-                selectables = "'" + s + "'";
-            }
+        
 
             sqlcmd = string.Format("SELECT {0} FROM {1} WHERE {2}", selectables, table, ArgsWhere);
 
